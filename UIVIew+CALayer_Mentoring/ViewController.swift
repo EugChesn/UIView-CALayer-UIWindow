38ba35e3deded0584aabbb212375164f7fb6bad8
Evgenii_Chistiakov <chesnok297@gmail.com>
2021-03-01 21:41:14 +0300

    Initial Commit

---
+++ UIVIew+CALayer_Mentoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIVIew+CALayer_Mentoring
+//
+//  Created by EvgeniiChistyakov on 01.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
